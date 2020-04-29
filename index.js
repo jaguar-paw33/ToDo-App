3faa15fa6785814ab6ff3102fe14a30a647c0527
jaguar_paw33 <mr.priyankmishra@gmail.com>
2020-04-29 23:36:45 +0530

    start server

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const port = 8000;
+
+
+
+
+
+
+app.listen(port, (err)=>{
+    if(err){
+        console.log(`Error in starting Server`);
+        return;
+    }
+    console.log(`Server is up and running at Port :: ${port}`);
+})
