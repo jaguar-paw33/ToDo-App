3efb0bc437b713a54361c99dc5e69069287e710b
jaguar-paw33 <mr.priyankmishra@gmail.com>
2021-07-25 00:34:27 +0530

    setup push to heroku

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/todo_db');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/todo_db');
 
 const db = mongoose.connection;
 
